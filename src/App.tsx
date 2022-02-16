f82c81348b2951e4db9898d747a4b1398da46f7b
JINDBS <jean.duquenne.ch@gmail.com>
2022-02-16 07:43:45 +0100

    cleaned up app and added toastified and router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import ContextProvider from './SolanaWallet/ContextProvider';
-import ConnectionButton from './SolanaWallet/ConnectionButton';
+import ContextProvider from './components/SolanaWallet/ContextProvider';
+import ConnectionButton from './components/SolanaWallet/ConnectionButton';
 
 function App(): JSX.Element {
   return (
     <ContextProvider>
-      <div className="App">
-        <header className="App-header">
-          <ConnectionButton />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ConnectionButton />
+      </BrowserRouter>
     </ContextProvider>
   );
 }
