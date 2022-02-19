24bc86b74694091eaea04c2539cf75cd9af1152d
JINDBS <jean.duquenne.ch@gmail.com>
2022-02-19 02:26:05 +0100

    removed layout because more confusing than anything else

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import { BrowserRouter, Routes, Navigate, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { RecoilRoot } from 'recoil';
 
 import AuthManager from 'components/Authentication/AuthManager';
+import WalletContextProvider from 'components/SolanaWallet/WalletContextProvider';
+
 import HomePage from 'pages/HomePages/HomePage';
 
-import Layout from 'components/Common/Layout';
 import WalletButton from 'components/SolanaWallet/WalletButton';
 
 import 'react-toastify/dist/ReactToastify.css';
 
 function App(): JSX.Element {
   return (
-    <Layout>
-      <BrowserRouter>
-        <ToastContainer
-          position="bottom-right"
-          autoClose={5000}
-          hideProgressBar
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <AuthManager>
-          <WalletButton />
-          <Routes>
-            <Route path="/Home" element={<HomePage />} />
-            {/* This route will redirect if Auth */}
-            <Route path="/" element={<Navigate to="/Home" />} />
-          </Routes>
-        </AuthManager>
-      </BrowserRouter>
-    </Layout>
+    <WalletContextProvider>
+      <RecoilRoot>
+        <BrowserRouter>
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <AuthManager>
+            <WalletButton />
+            <Routes>
+              <Route path="/Home" element={<HomePage />} />
+              {/* This route will redirect if Auth */}
+              <Route path="/" element={<Navigate to="/Home" />} />
+            </Routes>
+          </AuthManager>
+        </BrowserRouter>
+      </RecoilRoot>
+    </WalletContextProvider>
   );
 }
 
